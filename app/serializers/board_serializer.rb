9a2e28cb5b2a65c20554096926b76f156c294dc7
gudao.luo@gmail.com <gudao.luo@gmail.com>
2021-01-23 00:03:42 +0100

    Hello Rails + CORS for HTML

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,3 +1,7 @@
-class BoardSerializer < ActiveModel::Serializer
-  attributes :id
+class BoardSerializer<ActiveModel::Serializer
+  attributes :id, :name, :size, :surfer_id
+  
+  def surfer_id
+    self.object.surfer.name
+  end
 end
